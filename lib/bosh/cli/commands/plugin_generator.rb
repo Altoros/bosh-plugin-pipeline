53042d164fbee0a47b2812b631963906a19fb43e
A.S. Lomoff <lomov.as@gmail.com>
2015-01-13 14:06:02 +0300

    fix option names in command

--- lib/bosh/cli/commands/plugin_generator.rb
+++ lib/bosh/cli/commands/plugin_generator.rb
@@ -7,10 +7,10 @@ module Bosh::Cli::Command
     # example: bosh generate plugin bosh-magic
     usage "generate plugin"
     desc "Generates folder with skeleton of new BOSH plugin."
-    option "--author[=author]", String, "name of the owner (by default will take from git settings)"
-    option "--email[=email]", String, "email of the owner (by default will take from git settings)"
-    option "--description[=text]", String, "Short description of the plugin"
-    option "--license[=type]", String, "type of license file included to plugin repository" +
+    option "--author [author]", String, "name of the owner (by default will take from git settings)"
+    option "--email [email]", String, "email of the owner (by default will take from git settings)"
+    option "--description [text]", String, "Short description of the plugin"
+    option "--license [type]", String, "type of license file included to plugin repository" +
                                        "(possible values mit|gpl|lgpl|apache2)"
     def perform(plugin_name)
       puts options
